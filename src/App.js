dece7f3e33857a5846487708040c59f9f3df3346
unknown <colonylyfe@gmail.com>
2023-11-07 21:11:48 -0500

    webgl init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    const canvas = document.getElementById('webgl');
+    const gl = canvas.getContext('webgl');
+
+    // Clear the canvas
+    gl.clearColor(0.5, 0.5, 0.5, 0.9);
+
+    // Enable the depth test
+    gl.enable(gl.DEPTH_TEST);
+
+    // Clear the color buffer bit
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    /*======== Defining and storing the geometry ===========*/
+
+    var vertices = [
+      -0.5,0.5,0.0,
+      -0.5,-0.5,0.0,
+      0.5,-0.5,0.0, 
+    ];
+
+    const indices = [0,1,2];
+
+    // Create an empty buffer object to store vertex buffer
+    var vertex_buffer = gl.createBuffer();
+
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+    // Pass the vertex data to the buffer
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    // Unbind the buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    // Create an empty buffer object to store Index buffer
+    var Index_Buffer = gl.createBuffer();
+
+    // Bind appropriate array buffer to it
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
+    // Pass the vertex data to the buffer
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+    // Unbind the buffer
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+    /*================ Shaders ====================*/
+
+    // Vertex shader source code
+    var vertCode =
+    'attribute vec3 coordinates;' +
+        
+    'void main(void) {' +
+        ' gl_Position = vec4(coordinates, 1.0);' +
+    '}';
+
+    // Create a vertex shader object
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+
+    // Attach vertex shader source code
+    gl.shaderSource(vertShader, vertCode);
+
+    // Compile the vertex shader
+    gl.compileShader(vertShader);
+
+    //fragment shader source code
+    var fragCode =
+    'void main(void) {' +
+        ' gl_FragColor = vec4(0.0, 0.0, 0.0, 0.1);' +
+    '}';
+
+    // Create fragment shader object
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    // Attach fragment shader source code
+    gl.shaderSource(fragShader, fragCode); 
+
+    // Compile the fragmentt shader
+    gl.compileShader(fragShader);
+
+    // Create a shader program object to store
+    // the combined shader program
+    var shaderProgram = gl.createProgram();
+
+    // Attach a vertex shader
+    gl.attachShader(shaderProgram, vertShader);
+
+    // Attach a fragment shader
+    gl.attachShader(shaderProgram, fragShader);
+
+    // Link both the programs
+    gl.linkProgram(shaderProgram);
+
+    // Use the combined shader program object
+    gl.useProgram(shaderProgram);
+
+    /*======= Associating shaders to buffer objects =======*/
+
+    // Bind vertex buffer object
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+    // Bind index buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, Index_Buffer);
+
+    // Get the attribute location
+    var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+
+    // Point an attribute to the currently bound VBO
+    gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); 
+
+    // Enable the attribute
+    gl.enableVertexAttribArray(coord);
+
+    /*=========Drawing the triangle===========*/
+
+    // Set the view port
+    // gl.viewport(0,0,canvas.width,canvas.height);
+
+    // Draw the triangled
+    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT,0);
+
+    console.log('errors: ', gl.getError())
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <canvas id="webgl" width="400" height="400" style={{ border: '1px solid black'}}></canvas>
     </div>
   );
 }
